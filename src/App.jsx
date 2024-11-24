ec85bc66425198fd7c084a8734f36dd0864cb421
Abhishek singh <abhiramgarhia13@gmail.com>
2024-11-24 15:31:47 +0530

    update with database

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-import "./App.css"
-import Header from "./components/common/header/Header"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import About from "./components/about/About"
-import CourseHome from "./components/allcourses/CourseHome"
-import Team from "./components/team/Team"
-import Pricing from "./components/pricing/Pricing"
-import Blog from "./components/blog/Blog"
-import Contact from "./components/contact/Contact"
-import Footer from "./components/common/footer/Footer"
-import Home from "./components/home/Home"
-import Read from "../src/components/Pdf/Read"
-// import Write from "./Write"
+import "./App.css";
+import Header from "./components/common/header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/about/About";
+import CourseHome from "./components/allcourses/CourseHome";
+import Team from "./components/team/Team";
+import Pricing from "./components/pricing/Pricing";
+import Blog from "./components/blog/Blog";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/common/footer/Footer";
+import Home from "./components/home/Home";
+import Read from "../src/components/Pdf/Read";
 
 function App() {
   return (
-    <>
-      <Router>
-      <Route exact path='/read' element={<Read/>} />
-        <Header />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/about' element={<About />} />
-          <Route exact path='/courses' element={<CourseHome />} />
-          <Route exact path='/team' element={<Team />} />
-          <Route exact path='/pricing' element={<Pricing />} />
-          <Route exact path='/journal' element={<Blog />} />
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/read' element={<Read/>} />
-          
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/read" element={<Read />} /> {/* Read route without header/footer */}
+        <Route path="/*" element={
+          <>  {/* All other routes get header/footer */}
+            <Header />
+            <Routes> {/* Nested routes for other pages */}
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+              <Route exact path="/courses" element={<CourseHome />} />
+              <Route exact path="/team" element={<Team />} />
+              <Route exact path="/pricing" element={<Pricing />} />
+              <Route exact path="/journal" element={<Blog />} />
+              <Route exact path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </>
+        }/>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
